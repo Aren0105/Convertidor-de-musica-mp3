bc035cab7522a6c9d0e13de4ec0f1dfec7b9336e
Aren0105 <aren200501@gmail.com>
2026-08-27 10:22:42 -0500

    Refactorizacion de audio
    formato mp3 y mp4 actualizacion

--- downloader/youtube.py
+++ downloader/youtube.py
@@ -5,30 +5,37 @@ import yt_dlp
 def descargar_audio(url, format_type, download_folder):
 
     opciones = {
-
-        'format': 'bestaudio/best',
-
         'outtmpl': os.path.join(
             download_folder,
             '%(title)s.%(ext)s'
         ),
-
         'noplaylist': True,
-
         'cookiefile': 'cookies.txt'
     }
 
+    # MP3
     if format_type == 'mp3':
 
-        opciones['postprocessors'] = [
+        opciones['format'] = 'bestaudio/best'
 
-            {
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192'
-            }
+        opciones['postprocessors'] = [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
+        }]
 
-        ]
+    # MP4
+    elif format_type == 'mp4':
+
+        opciones['format'] = (
+            'bestvideo[ext=mp4]+bestaudio[ext=m4a]/'
+            'best[ext=mp4]'
+        )
+
+        opciones['merge_output_format'] = 'mp4'
+
+    else:
+        raise ValueError('Formato no válido')
 
     with yt_dlp.YoutubeDL(opciones) as ydl:
 
@@ -39,8 +46,12 @@ def descargar_audio(url, format_type, download_folder):
 
         filename = ydl.prepare_filename(info)
 
+    # Después de convertir a MP3, cambia la extensión
     if format_type == 'mp3':
-
         filename = os.path.splitext(filename)[0] + '.mp3'
+
+    # En MP4 yt-dlp puede haber generado el archivo .mp4
+    elif format_type == 'mp4':
+        filename = os.path.splitext(filename)[0] + '.mp4'
 
     return filename
